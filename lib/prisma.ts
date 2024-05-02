8336ef2372b58c930d4724a6e0736d6127fffd81
mmvergara <mark.jesusmanabat@gmail.com>
2024-05-02 19:29:18 +0800

    Add Prisma client singleton
    This commit adds a singleton for the Prisma client in the `lib/prisma.ts` file. The singleton ensures that only one instance of the Prisma client is created and used throughout the application.

---
+++ lib/prisma.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from "@prisma/client";
+
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+// eslint-disable-next-line
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+export default prisma;
