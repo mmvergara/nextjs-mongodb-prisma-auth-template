3eb2d41e91a51b0183eb3ec07aaa50c7715a2463
mmvergara <mark.jesusmanabat@gmail.com>
2024-05-02 19:29:05 +0800

    Update import path for auth handlers

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,2 @@
-import { handlers } from "@/auth"; // Referring to the auth.ts we just created
+import { handlers } from "@/lib/auth"; // Referring to the auth.ts we just created
 export const { GET, POST } = handlers;
