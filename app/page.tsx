6a7e40556b90afe89db6a1b30bb96695ef2ce242
mmvergara <mark.jesusmanabat@gmail.com>
2024-05-02 19:29:01 +0800

    Update page title in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-
-
 export default function Home() {
   return (
     <main className="flex items-center justify-center flex-col gap-4 pt-[10vh] text-white">
       <h1 className="text-5xl text-center bg-[hsl(0,0%,7%)] p-4 rounded-sm font-semibold">
-        NextJS + MongoDB + Prisma Template
+        NextJS + MongoDB + Prisma + Auth Template
       </h1>
 
       <form className="flex flex-col gap-2">
