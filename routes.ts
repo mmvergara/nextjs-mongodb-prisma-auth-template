81ad02a2bdbbd2147958f4a072ef035b8e7ba115
mmvergara <mark.jesusmanabat@gmail.com>
2024-05-03 11:30:26 +0800

    Add routes.ts file with public and auth routes

---
+++ routes.ts
@@ -0,0 +1,11 @@
+/**
+ * Public routes
+ * @type {string[]}
+ */
+export const publicRoutes = ["/", "/sign-in", "/sign-up"];
+
+export const authRoutes = ["/sign-in", "/sign-up"];
+
+export const apiAuthPrefix = "/api/auth";
+
+export const DEFAULT_LOGGED_IN_REDIRECT = "/settings";
