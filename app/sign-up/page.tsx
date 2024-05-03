914867a432187b09ca1a7de0e74acd84c313d982
mmvergara <mark.jesusmanabat@gmail.com>
2024-05-03 11:30:44 +0800

    Add sign-up form and validation logic

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,25 +1,66 @@
+"use client";
+import * as z from "zod";
+import { signUpSchema } from "@/lib/zod";
+import { startTransition, useState } from "react";
+import { signUpAction } from "@/lib/actions";
+import { useRouter } from "next/navigation";
+
 export default function SignUpPage() {
+  const router = useRouter();
+  const [error, setError] = useState<string>("");
+  const [formValues, setFormValues] = useState<z.infer<typeof signUpSchema>>({
+    email: "",
+    password: "",
+    username: "",
+  });
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError("");
+    const { error } = await signUpSchema.safeParseAsync(formValues);
+    if (error) return setError(error.issues[0].message);
+
+    startTransition(() => {
+      signUpAction(formValues).then((res) => {
+        if (res?.error) return setError(res.error);
+        router.push("/sign-in");
+      });
+    });
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormValues((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
   return (
     <main className="flex items-center justify-center flex-col gap-4 pt-[10vh] text-white">
       <h1 className="text-5xl text-center bg-[hsl(0,0%,7%)] p-4 rounded-sm font-semibold">
         Sign Up Page
       </h1>
-
-      <form className="flex flex-col gap-2">
+      <p className="text-red-500">{error}</p>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-2">
         <input
           type="text"
-          placeholder="Enter your name"
+          name="username"
+          value={formValues.username}
+          onChange={handleInputChange}
+          placeholder="username"
           className="p-2 rounded-md outline-none drop-shadow-sm bg-[hsl(0,0%,10%)]"
         />
 
         <input
           type="email"
+          name="email"
+          value={formValues.email}
+          onChange={handleInputChange}
           placeholder="Enter your email"
           className="p-2 rounded-md outline-none drop-shadow-sm bg-[hsl(0,0%,10%)]"
         />
 
         <input
           type="password"
+          name="password"
+          value={formValues.password}
+          onChange={handleInputChange}
           placeholder="Enter your password"
           className="p-2 rounded-md outline-none drop-shadow-sm bg-[hsl(0,0%,10%)]"
         />
