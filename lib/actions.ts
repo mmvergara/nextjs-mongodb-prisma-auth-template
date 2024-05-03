84a21a90ec5c36d9fb67f31033c85d08ffcff0e2
mmvergara <mark.jesusmanabat@gmail.com>
2024-05-03 11:30:46 +0800

    Refactor authentication actions and add sign up action

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,21 +1,43 @@
-import { AuthError } from "next-auth";
+"use server";
 import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
+import prisma from "./prisma";
+import { SignInValues, SignUpValues } from "./zod";
+import { hashSync } from "bcryptjs";
+import { PrismaClientValidationError } from "@prisma/client/runtime/library";
 
-export async function authenticate(
-  prevState: string | undefined,
-  formData: FormData
-) {
+export const signInAction = async (signInValues: SignInValues) => {
   try {
-    await signIn("credentials", formData);
+    await signIn("credentials", signInValues);
+    return { error: null };
   } catch (error) {
+    console.log("ERRORR OCCURED SIGNIN ACTION", error);
     if (error instanceof AuthError) {
+      console.log(error.type);
       switch (error.type) {
         case "CredentialsSignin":
-          return "Invalid credentials.";
+          return { error: "Invalid credentials" };
         default:
-          return "Something went wrong.";
+          return { error: "An error occurred" };
       }
     }
     throw error;
   }
-}
+};
+
+export const signUpAction = async (signUpValues: SignUpValues) => {
+  try {
+    await prisma.user.create({
+      data: {
+        ...signUpValues,
+        password: hashSync(signUpValues.password, 10),
+      },
+    });
+  } catch (error) {
+    const e = error as PrismaClientValidationError;
+    console.log(e);
+    console.log(e.cause);
+    console.log(e.message);
+    return { error: "An error occurred" };
+  }
+};
