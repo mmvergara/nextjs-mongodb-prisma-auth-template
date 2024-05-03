1b7cdc559cb3d74bd1a849b5f1318f9b49b13b2a
mmvergara <mark.jesusmanabat@gmail.com>
2024-05-03 14:48:28 +0800

    Refactor auth.ts: Remove unused callbacks

--- auth.ts
+++ auth.ts
@@ -5,20 +5,6 @@ import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  callbacks: {
-    async session({ token, session }) {
-      if (token.sub) {
-        session.user.id = token.sub;
-      }
-
-      return session;
-    },
-    async jwt({ token }) {
-      console.log("jwt callback", token);
-      return token;
-    },
-  },
-
   adapter: PrismaAdapter(prisma) as Adapter,
   session: { strategy: "jwt" },
   ...authConfig,
