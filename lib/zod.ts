fb40cb266a335b9f5e622cd82f450b9eb8aaede3
mmvergara <mark.jesusmanabat@gmail.com>
2024-05-03 11:30:51 +0800

    Update zod imports and add sign up schema

--- lib/zod.ts
+++ lib/zod.ts
@@ -1,4 +1,5 @@
-import { object, string } from "zod";
+import * as z from "zod";
+const { object, string } = z;
 
 export const signInSchema = object({
   email: string({ required_error: "Email is required" })
@@ -9,3 +10,19 @@ export const signInSchema = object({
     .min(8, "Password must be more than 8 characters")
     .max(32, "Password must be less than 32 characters"),
 });
+export type SignInValues = z.infer<typeof signInSchema>;
+
+
+export const signUpSchema = object({
+  email: string({ required_error: "Email is required" })
+    .min(1, "Email is required")
+    .email("Invalid email"),
+  password: string({ required_error: "Password is required" })
+    .min(1, "Password is required")
+    .min(8, "Password must be more than 8 characters")
+    .max(32, "Password must be less than 32 characters"),
+  username: string({ required_error: "Username is required" })
+    .min(4, "Username is required")
+    .max(32, "Username must be less than 32 characters"),
+});
+export type SignUpValues = z.infer<typeof signUpSchema>;
