979a35f36ed753776f6b60f6ee397f6ba673bff6
mmvergara <mark.jesusmanabat@gmail.com>
2024-06-13 16:19:30 +0800

    Refactor middleware.ts to improve code readability and add comments

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,6 @@
+import { NextResponse } from "next/server";
 import { auth } from "./auth";
-import {
-  DEFAULT_LOGGED_IN_REDIRECT,
-  apiAuthPrefix,
-  authRoutes,
-  publicRoutes,
-} from "./routes";
+import { apiAuthPrefix, authRoutes, publicRoutes } from "./routes";
 
 export default auth((req) => {
   const { nextUrl } = req;
@@ -14,17 +10,23 @@ export default auth((req) => {
   const isPublicRotue = publicRoutes.includes(nextUrl.pathname);
   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
+  // if it is an API Next Auth route, we don't want to redirect
   if (isApiAuthRoute) return;
 
   if (isAuthRoute) {
     if (isLoggedIn) {
-      return Response.redirect(new URL(DEFAULT_LOGGED_IN_REDIRECT, nextUrl));
+      // if the user is already logged in and is in sign-in or sign-up page
+      // redirect to the default logged in page (which is dashboard in this case)
+      console.log("redirecting to dashboard");
+      return NextResponse.redirect(new URL("/dashboard", nextUrl));
     }
+    // if the user is not logged in and is in sign-in or sign-up page, let them be
     return;
   }
 
   if (!isLoggedIn && !isPublicRotue) {
-    return Response.redirect(new URL("/sign-in", nextUrl));
+    // if the user is not logged in and is not in a public route, redirect to sign-in page
+    return NextResponse.redirect(new URL("/sign-in", nextUrl));
   }
 
   return;
