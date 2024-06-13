d1b6adb2a0de73d80d61a4c41ffedc07e5a13017
mmvergara <mark.jesusmanabat@gmail.com>
2024-06-13 16:19:38 +0800

    Remove DEFAULT_LOGGED_IN_REDIRECT constant

--- routes.ts
+++ routes.ts
@@ -7,5 +7,3 @@ export const publicRoutes = ["/", "/sign-in", "/sign-up"];
 export const authRoutes = ["/sign-in", "/sign-up"];
 
 export const apiAuthPrefix = "/api/auth";
-
-export const DEFAULT_LOGGED_IN_REDIRECT = "/dashboard";
