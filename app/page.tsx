4c2ca59f1c1cb94cf3fe342e4adac34b4dbe53e9
mmvergara <mark.jesusmanabat@gmail.com>
2024-06-13 16:02:30 +0800

    update landing page styling

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,22 @@ export default async function Home() {
   const session = await auth();
   return (
     <main className="flex items-center justify-center flex-col gap-4 pt-[10vh] text-white">
-      <h1 className="text-5xl text-center bg-[hsl(0,0%,7%)] p-4 rounded-sm font-semibold">
+      <h1 className="text-5xl text-center bg-[hsl(0,0%,7%)] p-4 px-8 rounded-sm font-semibold">
         NextJS + MongoDB + Prisma + Auth Template
       </h1>
-      <p>Session : {session ? session?.user?.email : "No session"}</p>
-      <section className="flex gap-4 flex-col items-center justify-center">
-        <Link href="/sign-in" className="p-2 bg-blue-700 rounded-lg px-6">
+      <section className="flex gap-4 flex-col items-center justify-center bg-zinc-800 p-8 rounded-md">
+        <p>Session : {session ? session?.user?.email : "No session"}</p>
+        <Link
+          href="/sign-in"
+          className="p-2 bg-[hsl(191,52%,30%)] hover:bg-[hsl(191,52%,35%)] rounded-sm px-6"
+        >
           Sign In
         </Link>
 
-        <Link href="/dashboard" className="p-2 bg-blue-700 rounded-lg px-6">
+        <Link
+          href="/dashboard"
+          className="p-2 bg-[hsl(191,52%,30%)] hover:bg-[hsl(191,52%,35%)] rounded-sm px-6"
+        >
           Dashboard Page (Protected)
         </Link>
       </section>
