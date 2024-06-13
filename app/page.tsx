d0aa348bd6e75cb1593aa6f8bf2169a340a60647
mmvergara <mark.jesusmanabat@gmail.com>
2024-06-13 16:19:43 +0800

    Update page styling

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,10 @@ export default async function Home() {
   const session = await auth();
   return (
     <main className="flex items-center justify-center flex-col gap-4 pt-[10vh] text-white">
-      <h1 className="text-5xl text-center bg-[hsl(0,0%,7%)] p-4 px-8 rounded-sm font-semibold">
+      <h1 className="text-5xl text-center bg-zinc-900 p-4 px-8 rounded-sm font-semibold">
         NextJS + MongoDB + Prisma + Auth Template
       </h1>
-      <section className="flex gap-4 flex-col items-center justify-center bg-zinc-800 p-8 rounded-md">
+      <section className="flex gap-4 flex-col items-center justify-center bg-zinc-900 p-8 rounded-sm">
         <p>Session : {session ? session?.user?.email : "No session"}</p>
         <Link
           href="/sign-in"
