73ffa51046c7d85bf6332e35eaec88fa1eca09c3
mmvergara <mark.jesusmanabat@gmail.com>
2024-06-13 16:19:50 +0800

    Refactor sign out functionality and update UI

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -1,28 +1,49 @@
 import { auth, signOut } from "@/auth";
+import { SubmitButton } from "@/components/SubmitButton";
+import {
+  isRedirectError,
+  redirect,
+} from "next/dist/client/components/redirect";
 import Link from "next/link";
 
 const DashboardPage = async () => {
   const session = await auth();
   return (
     <main className="flex items-center justify-center flex-col gap-4 pt-[10vh] text-white">
-      <h1 className="text-xl text-center bg-[hsl(0,0%,7%)] p-4 rounded-sm font-semibold">
-        This is a protected page
-      </h1>
-      <p>Hello {JSON.stringify(session?.user?.email)}</p>
       <Link
         href="/"
         className="bg-[hsl(0,0%,7%)] hover:bg-[hsl(0,0%,10%)] p-2 px-8 rounded-lg"
       >
         {"<-"} Home
       </Link>
-      <form
-        action={async () => {
-          "use server";
-          signOut({ redirectTo: "/" });
-        }}
-      >
-        <button type="submit">sign Out</button>
-      </form>
+      <section className="flex gap-4 flex-col items-center justify-center bg-zinc-900 p-8 rounded-sm">
+        <h1 className="text-xl text-center rounded-sm font-semibold">
+          This is a protected page
+        </h1>
+        <p>Hello {JSON.stringify(session?.user?.email)}</p>
+        <form
+          action={async () => {
+            "use server";
+            try {
+              await signOut({ redirect: false });
+            } catch (err) {
+              if (isRedirectError(err)) {
+                console.error(err);
+                throw err;
+              }
+            } finally {
+              redirect("/");
+            }
+          }}
+        >
+          <SubmitButton
+            pendingText="Signing out..."
+            className="p-2 px-4 mt-4 bg-[hsl(191,52%,30%)] hover:bg-[hsl(191,52%,35%)] rounded-sm"
+          >
+            Sign Out
+          </SubmitButton>
+        </form>
+      </section>
     </main>
   );
 };
