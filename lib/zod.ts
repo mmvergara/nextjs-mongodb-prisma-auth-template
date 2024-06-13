e252875a991d3a870c7fce432b6e6bcc53d39eec
mmvergara <mark.jesusmanabat@gmail.com>
2024-06-13 16:01:32 +0800

    Refactor validation schemas in zod.ts

--- lib/zod.ts
+++ lib/zod.ts
@@ -2,23 +2,16 @@ import * as z from "zod";
 const { object, string } = z;
 
 export const signInSchema = object({
-  email: string({ required_error: "Email is required" })
-    .min(1, "Email is required")
-    .email("Invalid email"),
+  email: string({ required_error: "Email is required" }).email("Invalid email"),
   password: string({ required_error: "Password is required" })
-    .min(1, "Password is required")
     .min(8, "Password must be more than 8 characters")
     .max(32, "Password must be less than 32 characters"),
 });
 export type SignInValues = z.infer<typeof signInSchema>;
 
-
 export const signUpSchema = object({
-  email: string({ required_error: "Email is required" })
-    .min(1, "Email is required")
-    .email("Invalid email"),
+  email: string({ required_error: "Email is required" }).email("Invalid email"),
   password: string({ required_error: "Password is required" })
-    .min(1, "Password is required")
     .min(8, "Password must be more than 8 characters")
     .max(32, "Password must be less than 32 characters"),
   username: string({ required_error: "Username is required" })
